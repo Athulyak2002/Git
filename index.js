e3e877448f3125691ab283b02aac11426d5c8db1
Athulyak2002 <148936733+Athulyak2002@users.noreply.github.com>
2024-02-05 10:58:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var flash = require('express-flash');
+var session = require('express-session');
+var mysql = require('mysql');
+var connection  = require('./lib/db');
+var usersRouter = require('./routes/users');
+var app = express();
 
-import Users from "./users";
-import Contact from "./contact";
-import Header from "./header";
-import Footer from "./footer";
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const routing = (
-  <Router>
-    <div>
-      <Header />
-      <hr />
-      <Switch>
-        
-        <Route path="/users" component={Users} />
-        <Route path="/contact" component={Contact} />
-      
-      </Switch>
-      <Footer />
-    </div>
-  </Router>
-);
+app.use(session({ 
+    cookie: { maxAge: 60000 },
+    store: new session.MemoryStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}))
 
-ReactDOM.render(routing, document.getElementById("root"));
+app.use(flash());
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.listen(3000);
