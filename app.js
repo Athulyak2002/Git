23aa19ee1eea4dd78fffd06ce10308364e644eae
Athulyak2002 <148936733+Athulyak2002@users.noreply.github.com>
2024-02-05 10:57:21 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,66 @@
-const express = require('express')
-const app = express()
-app.set('view engine', 'ejs')
-var usersRouter = require('./routes/users.js');
-app.use('/users', usersRouter);
-app.listen(3008);
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var validator=require('validator');
+var flash = require('express-flash');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var mysql=require('mysql');
+let app= require('express-validator');
+
+var conn = require('./lib/db');
+var authRouter = require('./routes/auth');
+let app=express();
+const port=3000;
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ 
+    secret: '123456cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}))
+
+app.use(flash());
+app.use(express-validator());
+app.use(validator());
+
+app.use(express.json());
+app.use(mysql());
+
+
+
+app.use('/auth', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(error, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = error.message;
+  res.locals.error = req.app.get('env') === 'development' ? error : {};
+
+  // render the error page
+  res.status(error.status || 500);
+  res.render('error');
+});
+// port must be set to 3000 because incoming http requests are routed from port 80 to port 8080
+app.listen( port,() =>{
+    console.log('Node app is running on port 3000')
+});
+module.exports=app;
